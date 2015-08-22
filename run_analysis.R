c2d936c18f851f4637b16723fdd61275ae33da5a
Sila Kissuu <sila@kissuu.com>
2015-08-22 16:44:17 -0500

    spelling

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,9 @@
 # This script:
-# 1. Merges the training and the test sets to create one data set.
+# 1. Merges the training and the test sets to create one dataset.
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-# 3. Uses descriptive activity names to name the activities in the data set
-# 4. Appropriately labels the data set with descriptive variable names. 
-# 5. From the data set in step 4, creates a second, independent tidy data set with the 
+# 3. Uses descriptive activity names to name the activities in the dataset
+# 4. Appropriately labels the dataset with descriptive variable names. 
+# 5. From the dataset in step 4, creates a second, independent tidy dataset with the 
 #    average of each variable for each activity and each subject.
 
 # Set prerequisite packages 
@@ -19,17 +19,17 @@ features <- read.table("./UCI HAR Dataset/features.txt")[,2]
 # Create list of mean and standard deviation features
 targetFeatures <- grepl("mean\\(\\)|std\\(\\)", features)
 
-# Read in training test data sets
+# Read in training test datasets
 X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
-# Apply colum names to X data set
+# Apply colum names to X dataset
 names(X_test) = features
 
 # Extract only the measurements on the mean and standard deviation for each measurement.
 X_test = X_test[, targetFeatures]
 
-# Apply colum names to test and subject data sets
+# Apply colum names to test and subject datasets
 y_test[,2] <- activityLabels[y_test[,1]]
 names(y_test) <- c("ActivityNum", "ActivityLabel")
 names(subject_test) <- "Subject"
@@ -37,18 +37,18 @@ names(subject_test) <- "Subject"
 # Bind test column data
 test_data <- cbind(as.data.table(subject_test), y_test, X_test)
 
-# Read in training test data sets
+# Read in training test datasets
 X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
 
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-#Apply colum names to training data set
+#Apply colum names to training dataset
 names(X_train) <- features
 
 # Extract only the measurements on the mean and standard deviation for each measurement.
 X_train <- X_train[, targetFeatures]
 
-# Apply colum names to training and subject data sets
+# Apply colum names to training and subject datasets
 y_train[,2] <- activity_labels[y_train[,1]]
 names(y_train) <- c("ActivityNum", "ActivityLabel")
 names(subject_train) <- "Subject"
